a5769fcda10893d9d249db38165f0c0a32764efd
Sadiku Mustapha Eneye <mustyeneye@gmail.com>
2022-09-20 11:54:20 -0700

    cycle linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * check_cycle- function checks if a list is a cycle
+ * @list: linked list
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *head;
+	listint_t *current;
+
+	if (list == NULL)
+		return (0);
+	head = list;
+	if (head == NULL)
+		return (0);
+	current = head;
+	if (current == NULL)
+		return (0);
+	while (current != head)
+	{
+		if (current->next == NULL)
+		{
+			return (0);
+		}
+		current = current->next;
+	}
+	return (1);
+}
