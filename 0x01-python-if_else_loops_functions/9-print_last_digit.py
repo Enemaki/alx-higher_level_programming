b464a56d3e7ec4775fa5870bdf68939acb10020f
Sadiku Mustapha Eneye <mustyeneye@gmail.com>
2022-09-22 09:21:22 -0700

    print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        rem = number % -10
+        rem = number % 10
+        return(rem)
     else:
         rem = number % 10
-    return(rem)
+        return(rem)
