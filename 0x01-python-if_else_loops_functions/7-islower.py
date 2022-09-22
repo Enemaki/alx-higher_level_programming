4e99c41a2b31c680c0486c931f8890b1d93e7022
Sadiku Mustapha Eneye <mustyeneye@gmail.com>
2022-09-22 07:26:14 -0700

    check lower case

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    for a in range(ord('a'), ord('z')+1):
-        if c == chr(a):
-            return True
-    else:
-        return False
+    return(ord('a') <= ord(c) <= ord('z'))
