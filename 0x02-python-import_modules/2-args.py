5cfbe2f9d43c492d9a672ab1b019f40e2ed82119
Sadiku Mustapha Eneye <mustyeneye@gmail.com>
2022-09-24 09:15:41 -0700

    command line arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,10 @@
 if __name__ == "__main__":
     from sys import argv
     num = len(argv) - 1
-    if num == 0:
-        print("{:d} arguments.".format(num))
-    elif num == 1 and num <= 2:
-        print("{:d} argument:".format(num))
-    else:
-        print("{:d} arguments:".format(num))
+    print("{:d} {:s}{:s}".format(num,
+                                 "argument" if num <= 2 and num == 1
+                                 else "arguments",
+                                 "." if num == 0 else ":"))
     for a, s in enumerate(argv):
-        if num > 0:
+        if a > 0:
             print("{}: {}".format(a, s))
-
-
