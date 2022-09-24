ff8e1200c57b32bbf824e7ee287e0d9cbd98f6ba
Sadiku Mustapha Eneye <mustyeneye@gmail.com>
2022-09-24 08:45:45 -0700

    command line arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    def no_of_args(*argv):
-        if len(argv) == 0:
-            print("{} arguments.".format(len(argv)))
-        if len(argv) > 0:
-            i = 1
-            print("{} arguments.".format(len(argv)))
-            for arg in argv:
-                print("{}: {}".format(i, arg))
-                i += 1
+    from sys import argv
+    num = len(argv)
+    if len(argv) == 0:
+        print("{:d} arguments.".format(len(argv[0])))
+    if len(argv) > 0:
+        if len(argv) == 1:
+            print("{:d} argument:".format(len(argv))
+        else:
+            print("{:d} arguments:".format(len(argv)))
+        for i in range(1, num):
+            print("{:d}: {:s}".format(i, argv[i]))
+
+
