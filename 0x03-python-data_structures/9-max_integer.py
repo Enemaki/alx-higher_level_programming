b234c70dff6760a4a84ec15e0352f822deb629e7
Sadiku Mustapha Eneye <mustyeneye@gmail.com>
2022-09-27 08:27:55 -0700

    max integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if mylist == []:
+        return None
+    max_int = 0
+    for item in my_list:
+        if item > max_int:
+            max_int = item
+    return max_int
