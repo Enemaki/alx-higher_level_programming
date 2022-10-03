4c104c573efdeaea34d7834d96e4d15c8c5b90c9
Sadiku Mustapha Eneye <mustyeneye@gmail.com>
2022-10-03 06:08:55 -0700

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    a = my_list.index(search)
-    new_list = my_list
-    new_list[a] = replace
-    return new_list
+    return [replace if item == search else item for item in my_list]
