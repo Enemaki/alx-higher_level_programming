036e8339c70977836ceae126cc16cca2f4c6f7f6
Sadiku Mustapha Eneye <mustyeneye@gmail.com>
2022-10-11 06:17:25 -0700

    print type

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             a += 1
         except (ValueError, TypeError):
-            b += 1
+            i += 1
             continue
     print("")
     return (a)
