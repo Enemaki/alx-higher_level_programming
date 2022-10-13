a2880dab57776239316382aa1e449db79dad4ce5
Sadiku Mustapha Eneye <mustyeneye@gmail.com>
2022-10-13 07:37:37 -0700

    square cont

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,17 +5,13 @@ Square class definition
 
 
 class Square:
-
-   """
+    """
     Square class with private instance attribute size
     """
 
     def __init__(self, size=0):
         """
-
         Args:
-
             size: size of the square
-
         """
         self.__size = size
